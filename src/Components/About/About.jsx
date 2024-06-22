91bbaab24e4905a8ef9f8aa7821c9ea3e4737c87
Abhishek Kumar <abhishek955717@gmail.com>
2024-06-22 15:16:30 +0530

    new

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         <div className="about-right">
             <div className="about-para">
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum suscipit, ullam eveniet earum atque repudiandae eum doloribus beatae delectus corrupti odio rem reprehenderit! Assumenda, reprehenderit!
+                I'm Abhishek Kumar, a third-year student at IIT BHU with a passion for coding and building things. I love diving into the world of data structures and algorithms, finding creative solutions to complex problems. My interests extend to both web development and Android app development, where I enjoy bringing ideas to life through intuitive interfaces. I'm constantly learning and seeking new challenges, always eager to contribute my problem-solving skills and technical expertise to innovative projects.
                 </p>
             </div>
             <div className="about-skills">
